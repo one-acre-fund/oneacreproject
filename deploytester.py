71bd71bd75d4a9635c22b466ca87c0f231aa00d4
Yunhe (John) Wang <yhjw88@gmail.com>
2015-05-02 15:13:01 -0400

    Switched tester to PyQt4

--- deploytester.py
+++ deploytester.py
@@ -1,14 +1,18 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt4 import QtGui
 
-if __name__ == '__main__':
+def main():
     
-    app = QApplication(sys.argv)
+    app = QtGui.QApplication(sys.argv)
 
-    w = QWidget()
+    w = QtGui.QWidget()
     w.resize(250, 150)
     w.move(300, 300)
     w.setWindowTitle('Simple')
     w.show()
     
     sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
